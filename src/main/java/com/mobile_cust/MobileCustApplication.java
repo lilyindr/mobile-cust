98fa34f3d5432d504a76560dd8113b4dc58c21ed
lilyindr <lily@ppu.co.id>
2024-12-18 11:24:51 +0700

    first time

---
+++ src/main/java/com/mobile_cust/MobileCustApplication.java
@@ -0,0 +1,13 @@
+package com.mobile_cust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileCustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileCustApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobile_cust/MobileCustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobile_cust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileCustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
